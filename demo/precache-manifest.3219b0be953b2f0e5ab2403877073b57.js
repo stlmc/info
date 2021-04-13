97aa344e1f39206e414b9901bce303ce7fddca6b
svlab <rgyenr2@gmail.com>
2021-04-13 11:50:25 +0900

    test page

---
+++ demo/precache-manifest.3219b0be953b2f0e5ab2403877073b57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3db6272c11084812334061ba4882312e",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "e35ed71449cc6a065996",
+    "url": "/demo/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "a427d6e724ac02b0b7a1",
+    "url": "/demo/static/css/main.e3105745.chunk.css"
+  },
+  {
+    "revision": "e35ed71449cc6a065996",
+    "url": "/demo/static/js/2.7b069cad.chunk.js"
+  },
+  {
+    "revision": "923a93445da4455d8e7e236ec4fa5442",
+    "url": "/demo/static/js/2.7b069cad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a427d6e724ac02b0b7a1",
+    "url": "/demo/static/js/main.2b8aee4e.chunk.js"
+  },
+  {
+    "revision": "e3028179c848f757ab89",
+    "url": "/demo/static/js/runtime-main.21671177.js"
+  }
+]);
